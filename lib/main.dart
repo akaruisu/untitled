70ac56797155fc4694f9dd299b9829213aeb4526
akaruisu <jocm0723@gmail.com>
2022-07-21 11:17:03 +0800

    name change

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.yellow,
       ),
-      home: const MyHomePage(title: ' Demo Home Page'),
+      home: const MyHomePage(title: '  Home Page'),
     );
   }
 }
